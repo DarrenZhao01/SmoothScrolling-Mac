3ab6bd5efb41f7a7e1be076a894cd49d0638c4f5
DarrenZhao01 <zhaod579@gmail.com>
2026-06-04 18:27:19 -0700

    Initial Commit

---
+++ SmoothScroll/SmoothScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothScrollApp.swift
+//  SmoothScroll
+//
+//  Created by Darren Zhao on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SmoothScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
